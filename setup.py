aae487fbdff5a0eceab00a88d439fbbc290148e9
Bonny Philip <philipbonny18@gmail.com>
2023-02-05 15:47:19 +0530

    new setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,39 @@
-from setuptools import find_packages,setup   # find_packages within root folder (here its sensor folder). Setup will help on publishing and setting up the package
+from setuptools import setup, find_packages
 from typing import List
 
+# Declaring variables for setup functions
+PROJECT_NAME = "Scania_Sensor_Fault_Prediction"
+VERSION = "0.0.1"
+AUTHOR = "Bonny Philip"
+DESRCIPTION = "This is an end to end project for sensor fault prediction"
+
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements_list() -> List[str]:
+    """
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
+
 setup(
-    name="sensor",
-    version="0.0.1",
-    author="Bonny Philip",
-    author_email="philipbonny18@gmail.com",
-    packages= find_packages(),  # this will search for packages
-    install_requires=[]
+    name=PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESRCIPTION,
+    packages=find_packages(),
+    install_requires=get_requirements_list()
 )
 
 
